32f13230b2c62a7558ce7e3bedb0c0df5ecc340f
tacjin <60470514+tacjin@users.noreply.github.com>
2025-08-03 18:25:37 +0800

    Update config.js

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -4,11 +4,12 @@ const CONFIG = {
 
   HEO_HOME_BANNER_ENABLE: true,
 
-  HEO_SITE_CREATE_TIME: '2021-09-21', // 建站日期，用于计算网站运行的第几天
+  HEO_SITE_CREATE_TIME: '2023-09-21', // 建站日期，用于计算网站运行的第几天
 
   // 首页顶部通知条滚动内容，如不需要可以留空 []
   HEO_NOTICE_BAR: [
-    { title: '欢迎来到我的博客', url: 'https://jin.ink' }
+    { title: '欢迎来到我的技术笔记网站', url: 'https://jin.wiki' }
+    ,{ title: '祝您一切都好', url: 'https://jin.wiki' }
     //,{ title: '访问文档中心获取更多帮助', url: 'https://docs.tangly1024.com' }
   ],
 
@@ -20,10 +21,10 @@ const CONFIG = {
   // 英雄区(首页顶部大卡)
   HEO_HERO_TITLE_1: '分享编程',
   HEO_HERO_TITLE_2: '与大数据',
-  HEO_HERO_TITLE_3: 'Jin.ink',
+  HEO_HERO_TITLE_3: 'Jin.wiki',
   HEO_HERO_TITLE_4: '三省吾身',
   HEO_HERO_TITLE_5: '靡不有初，鲜克有终',
-  HEO_HERO_TITLE_LINK: 'https://jin.ink',
+  HEO_HERO_TITLE_LINK: 'https://jin.wiki',
   // 英雄区遮罩文字
   HEO_HERO_COVER_TITLE: '随便逛逛',
 
